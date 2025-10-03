53c278a3a0d958951dc681c869d520aea53b6d8f
s394588 <s394588@students.cdu.edu.au>
2025-10-03 10:55:57 +0545

    created a reusable button component

--- ui/components.py
+++ ui/components.py
@@ -1,1 +1,7 @@
 # will contains all the ui components
+import tkinter as tk
+
+# create a button function
+def create_button(parent, text, command):
+    button = tk.Button(parent, text=text, command=command)
+    return button
